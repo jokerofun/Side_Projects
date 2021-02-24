6412d41faf6cc2f4485a042bb56a362ec36021e2
Simeon Plamenov Kolev <1081492@ucn.dk>
2021-02-24 10:14:59 +0100

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+// export const environment = {
+//   production: true
+// };
+
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAY9qvD0qyChhoesDDrIkl8r-GBxAsGCd0",
+    authDomain: "personalwebsitedb-fbc62.firebaseapp.com",
+    databaseURL: "https://personalwebsitedb-fbc62.firebaseio.com",
+    projectId: "personalwebsitedb-fbc62",
+    storageBucket: "personalwebsitedb-fbc62.appspot.com",
+    messagingSenderId: "768379845197"
+  }
 };
