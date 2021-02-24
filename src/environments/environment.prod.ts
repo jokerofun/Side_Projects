7c443a5e3d416598f1d89cea9c102c64eacd92d4
Simeon Plamenov Kolev <1081492@ucn.dk>
2021-02-24 10:37:11 +0100

    Revert "fix"
    This reverts commit 6412d41faf6cc2f4485a042bb56a362ec36021e2.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,3 @@
-// export const environment = {
-//   production: true
-// };
-
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyAY9qvD0qyChhoesDDrIkl8r-GBxAsGCd0",
-    authDomain: "personalwebsitedb-fbc62.firebaseapp.com",
-    databaseURL: "https://personalwebsitedb-fbc62.firebaseio.com",
-    projectId: "personalwebsitedb-fbc62",
-    storageBucket: "personalwebsitedb-fbc62.appspot.com",
-    messagingSenderId: "768379845197"
-  }
+  production: true
 };
